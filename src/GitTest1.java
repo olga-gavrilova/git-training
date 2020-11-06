30d4c4f3f8e21d34db300142e08ec804a1d400da
Olga Gavrilova <olyagavrilova92@gmail.com>
2020-11-06 16:18:59 +0200

    Commit which should be revert

--- src/GitTest1.java
+++ src/GitTest1.java
@@ -1,5 +1,6 @@
 public class GitTest1 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+	System.out.println("Hello, World! I want to revert");
     }
 }
--- src/GitTest2.java
+++ src/GitTest2.java
@@ -1,5 +1,6 @@
 public class GitTest2 {
     public static void main(String[] args) {
         System.out.println("Hello, World2!");
+	System.out.println("Hello and revert!!");
     }
 }
